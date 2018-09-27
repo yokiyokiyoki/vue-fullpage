d8aa5f1f1b968f2909a188333eaca68fdfc4f6fd
youjie <youjie@hudongpai.com>
2018-09-27 21:43:46 +0800

    style(注释): 理解一下

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ export default {
     Vue.component("v-fullpage", fullpage);
   }
 };
-
+// 需要自行处理浏览器引入和模块引入的问题
 if (typeof window !== "undefined" && window.Vue) {
   window.Vue.component("v-fullpage", fullpage);
 }
