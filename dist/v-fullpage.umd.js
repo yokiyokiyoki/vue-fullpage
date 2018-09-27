ab6ce0baddd0c5fde59907deeb40b66882d1e5b9
youjie <youjie@hudongpai.com>
2018-09-27 20:35:24 +0800

    build(打包): 准备发布

--- dist/v-fullpage.umd.js
+++ dist/v-fullpage.umd.js
@@ -221,6 +221,17 @@ module.exports = function (it) {
 
 /***/ }),
 
+/***/ "254a":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_lib_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_fullpage_vue_vue_type_style_index_0_id_4fae5152_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("80f7");
+/* harmony import */ var _node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_lib_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_fullpage_vue_vue_type_style_index_0_id_4fae5152_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_lib_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_fullpage_vue_vue_type_style_index_0_id_4fae5152_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_mini_css_extract_plugin_dist_loader_js_ref_6_oneOf_1_0_node_modules_css_loader_index_js_ref_6_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_lib_index_js_ref_6_oneOf_1_2_node_modules_cache_loader_dist_cjs_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_fullpage_vue_vue_type_style_index_0_id_4fae5152_scoped_true_lang_css___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
 /***/ "2aba":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -633,6 +644,13 @@ module.exports = function (it, tag, stat) {
 
 /***/ }),
 
+/***/ "80f7":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
 /***/ "8378":
 /***/ (function(module, exports) {
 
@@ -1015,12 +1033,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2867f158-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/fullpage.vue?vue&type=template&id=00521dbd&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2867f158-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/fullpage.vue?vue&type=template&id=4fae5152&scoped=true&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{ref:"fullpage",staticClass:"fullpage-container",style:(_vm.styleObj)},[_c('div',{directives:[{name:"show",rawName:"v-show",value:(_vm.isShow),expression:"isShow"}],ref:"scrollContainer",staticClass:"scroll-container",staticStyle:{"transition":"all ease 1s"}},[_vm._t("section")],2)])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/fullpage.vue?vue&type=template&id=00521dbd&
+// CONCATENATED MODULE: ./src/components/fullpage.vue?vue&type=template&id=4fae5152&scoped=true&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/web.dom.iterable.js
 var web_dom_iterable = __webpack_require__("ac6a");
@@ -1044,11 +1062,11 @@ var web_dom_iterable = __webpack_require__("ac6a");
         deltaY: 0,
         styleObj: {
           position: 'absolute',
+          overflow: 'hidden',
           top: 0,
           bottom: 0,
           left: 0,
-          right: 0,
-          overflow: hidden
+          right: 0
         }
       },
       isShow: false
@@ -1151,6 +1169,9 @@ function throttle(fn, delay) {
 }
 // CONCATENATED MODULE: ./src/components/fullpage.vue?vue&type=script&lang=js&
  /* harmony default export */ var components_fullpagevue_type_script_lang_js_ = (fullpagevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./src/components/fullpage.vue?vue&type=style&index=0&id=4fae5152&scoped=true&lang=css&
+var fullpagevue_type_style_index_0_id_4fae5152_scoped_true_lang_css_ = __webpack_require__("254a");
+
 // CONCATENATED MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
 /* globals __VUE_SSR_CONTEXT__ */
 
@@ -1252,6 +1273,7 @@ function normalizeComponent (
 
 
 
+
 /* normalize component */
 
 var component = normalizeComponent(
@@ -1260,7 +1282,7 @@ var component = normalizeComponent(
   staticRenderFns,
   false,
   null,
-  null,
+  "4fae5152",
   null
   
 )
